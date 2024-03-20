8dd23fba70a94e6193712aae54d9281dc466a221
kk <897086121@qq.com>
2024-03-20 16:14:57 +0800

    python脚本简单修改为适配本地环境的版本

--- control/python/control_script.py
+++ control/python/control_script.py
@@ -2,13 +2,12 @@ import socket
 import random
 import time
 
-
 udpSocket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-monkeyHost = ('127.0.0.1',6666)
+monkeyHost = ('192.168.1.112', 6666)
 
-for instr in range(1,5):
-    for direction in range(0,2):
-        value = random.randrange(1,100000)
-        sendData = str(instr) +"," + str(direction) +","+ str(value) 
-        udpSocket.sendto(sendData.encode(),monkeyHost)
+for instr in range(1, 5):
+    for direction in range(0, 2):
+        value = random.randrange(1, 100000)
+        sendData = str(instr) + "," + str(direction) + "," + str(value)
+        udpSocket.sendto(sendData.encode(), monkeyHost)
         time.sleep(1)
--- control/python/simpleSend.py
+++ control/python/simpleSend.py
@@ -2,9 +2,8 @@ import socket
 import random
 import time
 
-
 udpSocket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-monkeyHost = ('192.168.99.103',6666)
+monkeyHost = ('192.168.1.112', 6666)
 
 #  instr
 #  1: config latency
@@ -19,8 +18,8 @@ monkeyHost = ('192.168.99.103',6666)
 #  value uint64_t
 
 instr = 1
-direction = 1
-value = 1234
+direction = 0
+value = 100000
 
-sendData = str(instr) +"," + str(direction) +","+ str(value) 
-udpSocket.sendto(sendData.encode(),monkeyHost)
+sendData = str(instr) + "," + str(direction) + "," + str(value)
+udpSocket.sendto(sendData.encode(), monkeyHost)
